8a2ca9f420d6b2329e399e05de1a87afc50586c9
kchateau <chateau14@outlook.com>
2020-12-01 19:08:52 -0800

    Solve day 1 question 2, and cleanup question 1
    Solved question 2 and changed the whole solution to question 1

--- 2020/day_1/main.py
+++ 2020/day_1/main.py
@@ -1,5 +1,8 @@
-class ReportRepair():
-        
+from itertools import combinations 
+import functools
+from functools import partial
+
+class ReportRepair():    
     def parse_input(filename):
         with open(filename, "r") as my_file:
             input_parsed = []
@@ -8,15 +11,19 @@ class ReportRepair():
 
             return input_parsed
 
-    # Given a list of ints and a target value, return the multiplication of 2 numbers in the list that add to the target
-    def doubles(numbers, target_number):
-        for i, number in enumerate(numbers[:-1]):
-            complementary = target_number - number
-            if complementary in numbers[i+1:]:
-                return(number * complementary)
+    def solve(func, val, numbers, target_num):
+        num_list = list(filter(partial(func, target_num=target_num), list(combinations(numbers, val))))
+
+        return functools.reduce(lambda x, y: x * y, num_list[0])
+
+    def doubles(val, target_num): 
+        return sum(val) == target_num
+
+    def triples(val, target_num): 
+        return sum(val) == target_num
 
     numbers = parse_input("input.txt")
-    target_number = 2020
+    target_num = 2020
 
-    ans = doubles(numbers, target_number)
-    print(ans)
+    print("Doubles: " + str(solve(doubles, 2, numbers, target_num)))
+    print("Triples: " + str(solve(triples, 3, numbers, target_num)))
