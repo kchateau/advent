91bc10bcbb9226ade5c96cddc021f29df305c34e
Kendra Chateau <kendrachateau@Kendras-Air.hitronhub.home>
2020-12-14 20:37:55 -0800

    Day 11 - a nightmare

--- 2020/day_11/main.py
+++ 2020/day_11/main.py
@@ -27,8 +27,117 @@ class SeatingSystem():
                     else:
                         new_seat_row_list[row_index][seat_index] = '#'
 
+    def iterate_seats_2(self, seat_row_list, new_seat_row_list):
+        for (row_index, seat_row) in enumerate(seat_row_list):
+            for (seat_index, seat) in enumerate(seat_row):
+                if seat is "L":
+                    adjacent_seats = []
+                    adjacent_seats.append(self.get_up(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_down(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_left(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_right(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_up_left(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_up_right(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_down_left(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_down_right(seat_row_list, row_index, seat_index, seat_row))
+                    if '#' not in adjacent_seats:
+                        new_seat_row_list[row_index][seat_index] = '#'
+                    else:
+                        new_seat_row_list[row_index][seat_index] = 'L'
 
-    def get_adjacent(self, seat_row_list, row_index, seat_index ,seat_row):
+                if seat is '#':
+                    adjacent_seats = []
+                    adjacent_seats.append(self.get_up(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_down(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_left(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_right(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_up_left(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_up_right(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_down_left(seat_row_list, row_index, seat_index, seat_row))
+                    adjacent_seats.append(self.get_down_right(seat_row_list, row_index, seat_index, seat_row))
+                    count_occupied = ''.join(adjacent_seats).count('#')
+                    if count_occupied >= 5:
+                        new_seat_row_list[row_index][seat_index] = 'L'
+                    else:
+                        new_seat_row_list[row_index][seat_index] = '#'
+
+    def get_up(self, seat_row_list, row_index, seat_index, seat_row):
+        while row_index is not 0: # Up
+            if seat_row_list[row_index - 1][seat_index] is not '':
+                return seat_row_list[row_index - 1][seat_index]
+            else:
+                row_index -= 1
+
+        return ''
+
+    def get_down(self, seat_row_list, row_index, seat_index, seat_row):
+        while row_index is not len(seat_row_list) - 1: # Down
+            if seat_row_list[row_index + 1][seat_index] is not '':
+                return seat_row_list[row_index + 1][seat_index]
+            else:
+                row_index += 1
+
+        return ''
+
+    def get_right(self, seat_row_list, row_index, seat_index, seat_row):
+        while seat_index < len(seat_row) - 1: # Right
+            if seat_row_list[row_index][seat_index + 1] is not '':
+                return seat_row_list[row_index][seat_index + 1]
+            else:
+                seat_index += 1
+
+        return ''
+    
+    def get_left(self, seat_row_list, row_index, seat_index, seat_row):
+        while seat_index > 0: # Left
+            if seat_row_list[row_index][seat_index - 1] is not '':
+                return seat_row_list[row_index][seat_index - 1]
+            else:
+                seat_index -= 1
+
+        return ''
+
+    def get_up_left(self, seat_row_list, row_index, seat_index, seat_row):
+        while row_index is not 0 and seat_index > 0: # Up Left
+            if seat_row_list[row_index - 1][seat_index - 1] is not '':
+                return seat_row_list[row_index - 1][seat_index - 1]
+            else:
+                row_index -= 1
+                seat_index -= 1
+
+        return ''
+    
+    def get_up_right(self, seat_row_list, row_index, seat_index, seat_row):
+        while row_index is not 0 and seat_index < len(seat_row) - 1: #Up Right
+            if seat_row_list[row_index - 1][seat_index + 1] is not '':
+                return seat_row_list[row_index - 1][seat_index + 1]
+            else:
+                row_index -= 1
+                seat_index += 1
+
+        return ''
+
+    def get_down_left(self, seat_row_list, row_index, seat_index, seat_row):
+        while row_index is not len(seat_row_list) - 1 and seat_index > 0: # Down Left
+            if seat_row_list[row_index + 1][seat_index - 1] is not '':
+                return seat_row_list[row_index + 1][seat_index - 1]
+            else:
+                row_index += 1
+                seat_index -= 1
+
+        return ''
+
+    def get_down_right(self, seat_row_list, row_index, seat_index, seat_row):
+        while row_index is not len(seat_row_list) - 1 and seat_index < len(seat_row) - 1: # Down Right
+            if seat_row_list[row_index + 1][seat_index + 1] is not '':
+                return seat_row_list[row_index + 1][seat_index + 1]
+            else:
+                row_index += 1
+                seat_index += 1
+
+        return ''
+
+    def get_adjacent(self, seat_row_list, row_index, seat_index, seat_row):
         adjacent_seats = []
         if row_index is not 0: # Up
             adjacent_seats.append(seat_row_list[row_index - 1][seat_index])
@@ -46,6 +155,7 @@ class SeatingSystem():
             adjacent_seats.append(seat_row_list[row_index + 1][seat_index - 1])
         if row_index is not len(seat_row_list) - 1 and seat_index < len(seat_row) - 1: # Down Right
             adjacent_seats.append(seat_row_list[row_index + 1][seat_index + 1])
+
         return adjacent_seats
 
     def create_empty_list_copy(self, list_to_copy):
@@ -56,24 +166,42 @@ class SeatingSystem():
                 row_list.append('')
             new_list.append(row_list)
         return new_list
-
-
-
-    def main(self):
-        seat_row_list = self.parse_input("input.txt")
-        seat_row_list_copy = self.parse_input("input.txt")
+    
+    def question_1(self, seat_list):
         while True:
-            empty_list = self.create_empty_list_copy(seat_row_list)
-            self.iterate_seats(seat_row_list, empty_list)
-            if seat_row_list == empty_list:
+            empty_list = self.create_empty_list_copy(seat_list)
+            self.iterate_seats(seat_list, empty_list)
+            if seat_list == empty_list:
                 count = 0
-                for seat_row in seat_row_list:
+                for seat_row in seat_list:
                     for seat in seat_row:
                         if seat is '#':
                             count += 1
-                print(count)
+                print("Question 1: " + str(count))
                 break
-            seat_row_list = empty_list
+            seat_list = empty_list
+        return
+
+    def question_2(self, seat_list):
+        while True:
+            empty_list = self.create_empty_list_copy(seat_list)
+            self.iterate_seats_2(seat_list, empty_list)
+            if seat_list == empty_list:
+                count = 0
+                for seat_row in seat_list:
+                    for seat in seat_row:
+                        if seat is '#':
+                            count += 1
+                print("Question 2: " + str(count))
+                break
+            seat_list = empty_list
+        return
+
+    def main(self):
+        seat_row_list = self.parse_input("input.txt")
+
+        self.question_1(seat_row_list)
+        self.question_2(seat_row_list)
 
 if __name__ == "__main__":
     SeatingSystem().main()
